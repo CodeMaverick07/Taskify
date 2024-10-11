55752f56d7de337ea677a83adc251929c9251a54
hemant jatal <hemantjatal59@gmail.com>
2024-10-11 14:38:08 +0530

    Refactor static file paths in build-manifest.json and app-build-manifest.json

--- app/api/cards/[cardId]/route.ts
+++ app/api/cards/[cardId]/route.ts
@@ -1,6 +1,5 @@
 import { auth } from "@clerk/nextjs/server";
 import { NextResponse } from "next/server";
-
 import { db } from "@/lib/db";
 
 export async function GET(
@@ -14,26 +13,31 @@ export async function GET(
       return new NextResponse("Unauthorized", { status: 401 });
     }
 
-    const card = await db.card.findUnique({
+    const card = await db.card.findFirst({
       where: {
         id: params.cardId,
         list: {
           board: {
-            orgId,
+            orgId, // Filter on orgId
           },
         },
       },
       include: {
         list: {
           select: {
-            title: true,
+            title: true, // Include list title
           },
         },
       },
     });
 
+    if (!card) {
+      return new NextResponse("Card not found", { status: 404 });
+    }
+
     return NextResponse.json(card);
   } catch (error) {
+    console.error("Error fetching card:", error); // Error logging
     return new NextResponse("Internal Error", { status: 500 });
   }
 }
